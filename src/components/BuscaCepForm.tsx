7da2f3714f566c7370633c049c4fc95f2f6d0036
joaogabriel333 <joao.gabriel.souzaiclououd.com@gmail.com>
2023-09-21 15:20:13 -0300

    emplementado cadastro de usuário

--- src/components/BuscaCepForm.tsx
+++ src/components/BuscaCepForm.tsx
@@ -1,5 +1,9 @@
 import { setUncaughtExceptionCaptureCallback } from 'process';
-import React, { useState , ChangeEvent, FormEvent, useEffect } from 'react';
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+
+import styles from "../App.module.css";
+import Header from './Header';
+import Footer from './Footer'
 
 const BuscaCep = () => {
 
@@ -8,43 +12,47 @@ const BuscaCep = () => {
     const [uf, setUf] = useState<string>("");
     const [erro, setErro] = useState<string>("");
 
-    
+
 
     const findCep = (e: FormEvent) => {
         e.preventDefault();
 
-        fetch('https://viacep.com.br/ws/'+cep+'/json',
-        {
-            method: 'GET'
-        }).then(response => response.json())
-        .then(
-            data => {
-                setLocalidade(data.localidade);
-                setCep(data.cep);
-                setUf(data.uf);
-            }
-        ).catch(error => {console.error("Erro:", error)});
-       
+        fetch('https://viacep.com.br/ws/' + cep + '/json',
+            {
+                method: 'GET'
+            }).then(response => response.json())
+            .then(
+                data => {
+                    setLocalidade(data.localidade);
+                    setCep(data.cep);
+                    setUf(data.uf);
+                }
+            ).catch(error => { console.error("Erro:", error) });
+
     }
 
     const submitForm = (e: ChangeEvent<HTMLInputElement>) => {
-        if(e.target.name === "cep"){
+        if (e.target.name === "cep") {
             setCep(e.target.value);
         }
     }
     return (
-<div>
-    <form onSubmit={findCep}>
-        <label htmlFor="cep">CEP</label>
-        <input type="text" name="cep" id="cep" 
-        onChange={submitForm} />
-        <input type="submit" value="Pesquisar" />
-    </form>
-    <p>Cidade: {localidade}</p> 
-    <p>Estado: {uf}</p> 
-    <p>CEP: {cep}</p> 
-
-</div>
+        <div>
+            <Header />
+            <main className={styles.main}>
+                <form onSubmit={findCep}>
+                    <label htmlFor="cep">CEP</label>
+                    <input type="text" name="cep" id="cep"
+                        onChange={submitForm} />
+                    <input type="submit" value="Pesquisar" />
+                </form>
+                <p>Cidade: {localidade}</p>
+                <p>Estado: {uf}</p>
+                <p>CEP: {cep}</p>
+                <p></p>
+            </main>
+            <Footer/>
+        </div>
     );
 }
 
